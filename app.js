d86a5a9cf9f4fd20b73d8dd87c95c1a3f76e9d21
Victor <mars.vic5@gmail.com>
2017-03-07 16:31:22 -0600

    Load places

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-var map = L.map('map').setView([33.4501001, -101.9107704], 4); // setup the map
+'use strict';
+
+// Program constants
+const START_COORDINATES = [33.4501001, -101.9107704];
+const START_ZOOM = 4;
+const MAX_ZOOM = 18;
+
+const map = L.map('map').setView(START_COORDINATES, START_ZOOM); // setup the map
+
 L.tileLayer('https://{s}.tiles.mapbox.com/v4/{user}.{mapId}/{z}/{x}/{y}.png?access_token={token}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+    maxZoom: MAX_ZOOM,
     mapId: 'i81oam9h',
     user: 'skycatch-dev',
     token: 'pk.eyJ1Ijoic2t5Y2F0Y2gtZGV2IiwiYSI6Ik1PVjVYNEkifQ.j2X9OOZDz7ABqUvHk4kesw'
@@ -9,7 +17,8 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v4/{user}.{mapId}/{z}/{x}/{y}.png?acce
 
 var markers = L.markerClusterGroup();
 
-markers.addLayer(L.marker([37.4501001, -121.9107704]).bindPopup('Keira River'));
-markers.addLayer(L.marker([28.3208337, -81.553712]).bindPopup('Willow Creek'));
-
+places.forEach((place) => {
+  markers.addLayer(L.marker([place.location.lat, place.location.lng]) // Position
+      .bindPopup(`<b>${place.id}</b> ${place.name}.`)); // Popup info
+});
 map.addLayer(markers);
